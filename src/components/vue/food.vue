5d04336dfd60bcc01cd2ea28a755882a6c6499cd
Nicholas <nic.j.merry@gmail.com>
2024-11-16 14:19:09 +0200

    added vue to project

---
+++ src/components/vue/food.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in food">
+        {{ item }}
+      </li>
+    </ul>
+    <v-btn>
+      Button
+    </v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+const food = ["Pizza", "French fries", "Spaghetti"];
+</script>
